6b2ceebe682a4da432d511487ccbbec3a334f637
Anthony Mai <anthony.mai530@gmail.com>
2019-11-15 03:12:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,19 @@
 def prime?(num)
-count=0
+  count = 0
 if num <= 0 || num == 1 || num < 0
 	false
 else
 	
 	i=2
 	while(i<num)
-		if (num%i==0)
+		if (num % i == 0)
 			count+=1
 		end
 		i+=1
 	end
 	
 end
-	if count>1
+	if count != 0
 		false
 	else
 		true
