66a95dbb166570e2e7cd15e2b3a3887ce1b9baa4
Anthony Mai <anthony.mai530@gmail.com>
2019-11-15 02:59:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,12 @@
 def prime?(num)
-  i = 1
-  if num <= 1 || num == 1 || num == 0
-    return false
-    elsif num == 2 
-    return true
-  while i < num
-  if num % i == 0 
-    return false
-    i += 1
-      end
+  if number <= 0 || number == 1
+    false
+  i = 2
+  while i < (num - 1)
+  if num % i == 0
+    false
     end
+  i +=1
   end
   true
 end
