eeefd9d07a899b64ecbb69daca199cf95016f541
Anthony Mai <anthony.mai530@gmail.com>
2019-11-15 03:14:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,10 @@
-def prime?(num)
-  count = 0
-if num <= 0 || num == 1 
-	false
-else
-	
-	i=2
-	while(i<num)
-		if (num % i == 0)
-			count+=1
-		end
-		i+=1
-	end
-	
-end
-	if count == 0 || num < 0
-		true
-	else
-		false
-	end
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0
+  end
+  end
 end
