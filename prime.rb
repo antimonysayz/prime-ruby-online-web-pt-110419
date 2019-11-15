d26c7f4bb6e71d2337e5255730b34b098c1e0ae2
Anthony Mai <anthony.mai530@gmail.com>
2019-11-15 03:06:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,20 @@ def prime?(number)
     if number <= 0 || number == 1
         return false
     end
-    
+    i=2
+	while(i<num)
+		if (num%i==0)
+			count+=1
+		end
+		i+=1
+	end
+	
+end
+	if count>1
+		false
+	else
+		true
+	end
 end
 
 prime?(1763)
