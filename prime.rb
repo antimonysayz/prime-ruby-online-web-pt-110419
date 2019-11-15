02d5209809127b5c41c11774993b09d76bc5cefd
Anthony Mai <anthony.mai530@gmail.com>
2019-11-15 02:40:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1 || num == 1 || num == 0
+    return false
+    elsif num == 2 
+    return true
+  elsif num % 2 == 0 
+    return false
+  end
+  true
+end
